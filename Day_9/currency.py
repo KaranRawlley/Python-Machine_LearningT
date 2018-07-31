ce1b8e55f306b83e020bdd6be474808a56257e5f
karan8798 <karan8798@gmail.com>
2018-07-31 05:59:29 +0530

    Recruitment Predictor App

--- Day_9/currency.py
+++ Day_9/currency.py
@@ -1,73 +1,143 @@
-class Currency:
-    def __init__(self,code,exchange_to_usd):
-        self.amount=0.00
-        self.code=code
-        self.exchange_to_usd=exchange_to_usd
-        
-    def set_amount(self,amount):
-        self.amount=amount
-    
-    def in_currency(self,amount):
-        return amount/self.exchange_to_usd
-    
-    def to_usd(self,amount=None):
-        to_convert=amount or self.amount
-        return to_covert * self.exchange_to_usd
-    
-    def __eq__(self,other):
-        # Exactly equal, ==
-        return self.to_usd() == other.to_usd()
-    
-    def __gt__(self,other):
-        # Greater than, >
-        return self.to_usd() > other.to_usd()
-    
-    def __lt__(self,other):
-        # Less than, <
-        pass
-    
-    def __le__(self,other):
-        # Less than or equal to, <=
-        pass
-    
-    def __ge__(self,other):
-        # Greater than or equal to, >=
-        pass
-    
-pounds=Currency("GBP",1.21)
-pounds.set_amount(1000)
-euros=Currency("EUR",1.07)
-euros.set_amount(1000)
+from tkinter import *
+import pandas as pd
+import numpy as np
+import seaborn as sb
+from sklearn.linear_model import LinearRegression
+from sklearn.cross_validation import train_test_split
+from sklearn import metrics
+from sklearn.linear_model import LogisticRegression
+from sklearn import tree
+from sklearn.ensemble import RandomForestClassifier
 
-print(pounds > euros)
+def to_binary(pred):
+    return int(pred > 0.5)
 
+def trainLinearReg(X,y):
+    model = LinearRegression()
+    model.fit(X,y)
+    return model
 
+def trainLogisticReg(X,y):
+    model = LogisticRegression()
+    model.fit(X,y)
+    return model
 
-
-
-# import code
-# class Currency:
-#     
-#     def __init__(self,code,change_to_usd):
-#         self.amount = 0.00
-#         self.code = code
-#         self.change_to_usd = change_to_usd
-#         
-#     def set_amount(self,amount):
-#         self.amount =   amount 
-#      
-#     def in_currency(self,amount):
-#         return amount/self.change_to_usd
-#     
-#     def to_usd(self,amount=None):
-#         to_convert  =   amount or self.amount 
-#         return to_convert * self.change_to_usd
-#     
-#     def __eq__(self,other):
-#         return self.to_usd()==other.to_usd()
-#     
-#     def __get__(self,other):
-#         return self.to_usd()>other.to_usd()
-#     
+def trainDecisionTree(X,y):
+    clf = tree.DecisionTreeClassifier()
+    clf = clf.fit(X,y)
+    return clf 
     
-              
+def trainRandomForest(X,y):
+    clf = RandomForestClassifier(n_estimators = 10)            
+    clf = clf.fit(X,y)
+    return clf
+a=1
+
+def click_chk():
+    myvals=[[e2_value.get(),e3_value.get(),e4_value.get(),e5_value.get(),e6_value.get()]]
+    print(myvals)
+    res = []
+    res.append(to_binary(modelln.predict(myvals)))
+    res.append(modellg.predict(myvals)[0])
+    res.append(modeldt.predict(myvals)[0])
+    res.append(modelrf.predict(myvals)[0])
+    v1.set(str(res[0]))
+    v2.set(str(res[1]))
+    v3.set(str(res[2]))
+    v4.set(str(res[3]))
+   # v5.set(str[4])    
+
+# df = pd.read_csv("PrepData1.csv")
+# features = list(df.columns[2:7])
+# y = df['Hired']
+# X = df[features]
+# 
+# modelln = trainLinearReg(X,y)
+# modellg = trainLogisticReg(X,y)
+# modeldt = trainDecisionTree(X,y)
+# modelrf = trainRandomForest(X,y)
+root=Tk()
+root.config(bg='#34495E')
+e1_value = StringVar()
+e2_value = DoubleVar()
+e3_value = DoubleVar()
+e4_value = IntVar()
+e5_value = DoubleVar()
+e6_value = DoubleVar()
+topFrame = Frame(root,bg='black')
+logo = PhotoImage(file="logo.png")
+w1 = Label(topFrame,image=logo,height=100,width=200).grid(row=1,column=6,columnspan=2,padx=50)  
+w2 = Label(topFrame,justify=LEFT,padx=10,text="Tech Company Pvt Ltd.",font=('times', 50, 'bold'),fg='#148F77').grid(row=1,column=1,padx=50,columnspan=2)
+topFrame.pack()
+middleFrame=Frame(root)
+
+label1 = Label(middleFrame,text='Name : ',font=25,padx=10,pady=10)
+label1.grid(row=1,column=1,padx=90,pady=10,sticky='W')
+entry1 = Entry(middleFrame,font=35,textvariable=e1_value)
+entry1.grid(row=1,column=2,padx=90,pady=10)
+
+label2 = Label(middleFrame,text='Percentage : ',font=25,padx=10,pady=10)
+label2.grid(row=2,column=1,padx=90,pady=10,sticky='W')
+entry2 = Entry(middleFrame,font=35,textvariable=e2_value)
+entry2.grid(row=2,column=2,padx=90,pady=10)
+
+label3 = Label(middleFrame,text='Backlog : ',font=25,padx=10,pady=10)
+label3.grid(row=3,column=1,padx=90,pady=10,sticky='W')
+entry3 = Entry(middleFrame,font=35,textvariable=e3_value)
+entry3.grid(row=3,column=2,padx=90,pady=10)
+
+label4 = Label(middleFrame,text='Internship : ',font=25,padx=10,pady=10)
+label4.grid(row=4,column=1,padx=90,pady=10,sticky='W')
+entry4 = Entry(middleFrame,font=35,textvariable=e4_value)
+entry4.grid(row=4,column=2,padx=90,pady=10)
+
+label5 = Label(middleFrame,text='First Round : ',font=25,padx=10,pady=10)
+label5.grid(row=5,column=1,padx=90,pady=10,sticky='W')
+entry5 = Entry(middleFrame,font=35,textvariable=e5_value)
+entry5.grid(row=5,column=2,padx=90,pady=10)
+
+label6 = Label(middleFrame,text='Communication Skills : ',font=25,padx=10,pady=10)
+label6.grid(row=6,column=1,padx=90,pady=10,sticky='W')
+entry6 = Entry(middleFrame,font=35,textvariable=e6_value)
+entry6.grid(row=6,column=2,padx=90,pady=10)
+
+rightFrame = Frame(root,bg="Black")
+button1 = Button(rightFrame,text="CHECK", fg='white',bg="#2E86C1",bd=4,font=80,padx=20,pady=5,command=click_chk)
+button1.grid(row=1,column=10)
+rightFrame.pack(side=RIGHT,padx=90)
+
+middleFrame.pack(pady=20,padx=20)
+bottomFrame = Frame(root)
+bottomFrame.config(bg='#34495E')
+label_res = Label(bottomFrame,text='Result:',font=55,padx=20,pady=10,bg='#34495E',fg='WHITE')
+label_res.grid(row=0,column=0)
+v1=v2=v3=v4=StringVar()
+rlabel1 = Label(bottomFrame,text='Linear Regression: ',font=25,padx=10,pady=10,bg='#34495E',fg='WHITE')
+rlabel1.grid(row=1,column=2)
+rlabel2 = Label(bottomFrame,text='Logistic Regression: ',font=25,padx=10,pady=10,bg='#34495E',fg='WHITE')
+rlabel2.grid(row=2,column=2)
+rlabel3 = Label(bottomFrame,text='Decision Tree: ',font=25,padx=10,pady=10,bg='#34495E',fg='WHITE')
+rlabel3.grid(row=3,column=2)
+rlabel4 = Label(bottomFrame,text='Random Forest: ',font=25,padx=10,pady=10,bg='#34495E',fg='WHITE')
+rlabel4.grid(row=4,column=2)
+#     rlabel5 = Label(bottomFrame,text='',font=25,padx=10,pady=10,bg='#34495E',fg='WHITE')
+#     rlabel5.grid(row=5,column=2)
+    
+rlabelv1 = Label(bottomFrame,textvariable=v1,font=25,padx=10,pady=10,bg='#34495E',fg='WHITE')
+rlabelv1.grid(row=1,column=3)
+rlabelv2 = Label(bottomFrame,textvariable=v2,font=25,padx=10,pady=10,bg='#34495E',fg='WHITE')
+rlabelv2.grid(row=2,column=3)
+rlabelv3 = Label(bottomFrame,textvariable=v3,font=25,padx=10,pady=10,bg='#34495E',fg='WHITE')
+rlabelv3.grid(row=3,column=3)
+rlabelv4 = Label(bottomFrame,textvariable=v4,font=25,padx=10,pady=10,bg='#34495E',fg='WHITE')
+rlabelv4.grid(row=4,column=3)
+#     rlabelv5 = Label(bottomFrame,textvariable='',font=25,padx=10,pady=10,bg='#34495E',fg='WHITE')
+#     rlabelv5.grid(row=5,column=2)
+    
+bottomFrame.pack(pady=10)
+
+
+
+
+root.mainloop()
+#---------------------------------------------------------------------------------------------------------
--- Projects/age_calculator.py
+++ Projects/age_calculator.py
@@ -3,10 +3,10 @@ import time
 dob = input("Enter DOB, use dd/mm/yyyy format: ")
 dob = datetime.strptime(dob, '%d/%m/%Y')
 print("Here are your age statistics...")
-time.sleep(2.4)
+time.sleep(2)
 print ("Years : %d" % ((datetime.today() - dob).days/365))
 print ("Months : %d" % ((datetime.today() - dob).days/30))
 print ("Hours : %d" % ((datetime.today() - dob).days*24))
 print ("Minutes : %d" % ((datetime.today() - dob).days*1440))
 print ("Seconds : %d" % ((datetime.today() - dob).days*86400))
-
+x=input("---------------------------------")
